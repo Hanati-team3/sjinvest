810495d5918f5026c7a4fbf469d288f0f4a38d14
kosta\uyk <meopulior@gmail.com>
2018-12-01 18:22:57 +0900

    pom api버전 수정, company 컨트롤러 list 추가

--- src/main/java/com/sjinvest/sos/company/controller/CompanyController.java
+++ src/main/java/com/sjinvest/sos/company/controller/CompanyController.java
@@ -1,6 +1,12 @@
 package com.sjinvest.sos.company.controller;
 
+import java.awt.PageAttributes.MediaType;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -23,6 +29,12 @@ public class CompanyController {
 	public String regist(Company company, RedirectAttributes rttr) {
 		log.info("register : "+company);
 		return "redirect:/company/list";
+	}
+	
+	@GetMapping("/list" )
+	public ResponseEntity<List<Company>> list() {
+		log.info("company list");
+		return new ResponseEntity<>(service.list(),HttpStatus.OK);
 	}
 
 }
