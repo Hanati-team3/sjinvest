27be7973e5f78da8196f8b2c2b3d99905d851863
gloomycloud <battlecloud@naver.com>
2018-12-04 12:28:27 +0900

    쌍따옴표 제거

--- src/main/webapp/resources/js/stock-common/stock-common.js
+++ src/main/webapp/resources/js/stock-common/stock-common.js
@@ -1,3 +1,12 @@
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+function removeLabelQuotes(data){
+    var index = 0;
+	var result =  data.label.forEach(function(element){
+		data.label[index] = element.replace(/\"/gi,"");
+		index = index +1;
+	});
+	return result;
+}
