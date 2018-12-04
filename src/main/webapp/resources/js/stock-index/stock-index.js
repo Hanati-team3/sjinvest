3cb4a4edf3f18e10edb87294064c33ce00e4963e
kosta\uyk <meopulior@gmail.com>
2018-12-04 11:57:40 +0900

    차트 수정중

--- src/main/webapp/resources/js/stock-index/stock-index.js
+++ src/main/webapp/resources/js/stock-index/stock-index.js
@@ -42,6 +42,7 @@ function indexUpdate() {
 		}),
 		success : function(stockData) {
 			console.log(stockData);
+			window.stock = stockData;
 			setFieldCard(stockData.fieldStock);
 			setInterestCard(stockData.interestCard);
 			 //setTimeout(indexUpdate, 2000);
@@ -73,11 +74,11 @@ function setInterestCard(interestCard) {
 		var stockDiv = $("<div class=\"company-stock\" data-swiper-parallax=\"-500\"></div>");
 		var dayBeforeSpan = $("<span class=\"indicator\"></span>");
 		var chartDiv = $("<div class=\"chart-js chart-js-line-stacked\">" +
-							"<canvas id=\"line-stacked-chart\" width=\"730\" height=\"300\"></canvas>" +
+							"<canvas name=\"line-stacked-chart\" width=\"730\" height=\"300\"></canvas>" +
 						"</div>");
 		$(nameDiv).text(interestCard.stockList[i].stockName);
 		$(stockDiv).text(numberWithCommas((interestCard.stockList[i].stockPrice)));
-		$(dayBeforeSpan).text("전일대비" + interestCard.stockList[i].stockChange + interestCard.stockList[i].stockDiff+"%");
+		$(dayBeforeSpan).text("전일대비 " + interestCard.stockList[i].stockChange +" "+ interestCard.stockList[i].stockDiff.toFixed(2)+"%");
 		if(interestCard.stockList[i].stockDiff > 0) {
 			$(stockDiv).removeClass('minus').addClass('plus');
 			$(dayBeforeSpan).removeClass('minus').addClass('plus');
@@ -86,6 +87,9 @@ function setInterestCard(interestCard) {
 			$(stockDiv).removeClass('plus').addClass('minus');
 			$(dayBeforeSpan).removeClass('plus').addClass('minus');
 		}
+		console.log("name"+ interestCard.stockList[i].stockName);
+		console.log("stockPrice"+ interestCard.stockList[i].stockPrice);
+		console.log("stockChange"+ interestCard.stockList[i].stockChange);
 		$(statisticsSlide).append(nameDiv);
 		$(statisticsSlide).append(stockDiv);
 		$(statisticsSlide).append(dayBeforeSpan);
@@ -93,79 +97,190 @@ function setInterestCard(interestCard) {
 		$(swiperSlide).append(statisticsSlide);
 		$(slideCard).append(swiperSlide);
 	}
-	$.getScript('../resources/js/main.js');
+	runInterestChart(interestCard.chartList);
+	CRUMINA.initSwiper();
+	
+//	$.getScript('../resources/js/main.js');
 //	$.getScript('../resources/js/ajax-pagination.js');
 //	$.getScript('../resources/js/swiper.jquery.min.js');
 //	$.getScript('../resources/js/run-chart.js');
 }
 
 /** 관심종목 차트 */
-var lineStackedChart = document.getElementById("line-stacked-chart");
-/*
- *  Lines Graphic
- * 14-FavouritePage-Statistics.html
- */
-if (lineStackedChart !== null) {
-    var ctx_ls = lineStackedChart.getContext("2d");
-    var data_ls = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [
-            {
-                label: " - Favorites",
-                backgroundColor: "rgba(57,169,255,0.35)",
-                borderColor: "#38a9ff",
-                borderWidth: 4,
-                pointBorderColor: "#38a9ff",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 4,
-                pointRadius: 6,
-                pointHoverRadius: 8,
-                data: [98,42,38,57,82,41,36,30,45,62,64,80]
-            },
-            {
-                label: " - Visitors",
-                backgroundColor: "rgba(8,221,123,0.2)",
-                borderColor: "#08ddc1",
-                borderWidth: 4,
-                pointBorderColor: "#08ddc1",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 4,
-                pointRadius: 6,
-                pointHoverRadius: 8,
-                data: [78,101,80,87,120,105,110,76,101,96,100,135]
-            }]
-    };
+function runInterestChart(chartList) {
+	console.log(chartList);
+	var lineStackedCharts = document.getElementsByName("line-stacked-chart");
+	window.charts = lineStackedCharts;
+	console.log("length : " +lineStackedCharts.length);
+	for (var i = 0; i < lineStackedCharts.length; i++) {
+		var lineStackedChart = lineStackedCharts[i];
+		var eachChart = chartList.pop();
+		console.log("aa");
+		window.aa = eachChart;
+		/*
+		 *  Lines Graphic
+		 * 14-FavouritePage-Statistics.html
+		 */
+		if (lineStackedChart !== null) {
+		    var ctx_ls = lineStackedChart.getContext("2d");
+		    var data_ls = {
+		        labels: eachChart.label,
+		        datasets: [
+		            {
+		                label: " - Won",
+		                backgroundColor: "rgba(57,169,255,0.35)",
+		                borderColor: "#38a9ff",
+		                borderWidth: 4,
+		                pointBorderColor: "#38a9ff",
+		                pointBackgroundColor: "#fff",
+		                pointBorderWidth: 4,
+		                pointRadius: 6,
+		                pointHoverRadius: 8,
+		                data: eachChart.data.pop()
+		            }]
+		    };
 
-    var lineStackedEl = new Chart(ctx_ls, {
-        type: 'line',
-        data: data_ls,
-        options: {
-            legend: {
-                display: false
-            },
-            responsive: true,
-            scales: {
-                xAxes: [{
-                    gridLines: {
-                        color: "#f0f4f9"
-                    },
-                    ticks: {
-                        fontColor: '#888da8'
-                    }
-                }],
-                yAxes: [{
-                    gridLines: {
-                        display: false
-                    },
-                    ticks: {
-                        beginAtZero:true,
-                        fontColor: '#888da8'
-                    }
-                }]
-            }
-        }
-    });
+		    var lineStackedEl = new Chart(ctx_ls, {
+		        type: 'line',
+		        data: data_ls,
+		        options: {
+		            legend: {
+		                display: false
+		            },
+		            responsive: true,
+		            scales: {
+		                xAxes: [{
+		                    gridLines: {
+		                        color: "#f0f4f9"
+		                    },
+		                    ticks: {
+		                        fontColor: '#888da8'
+		                    }
+		                }],
+		                yAxes: [{
+		                    gridLines: {
+		                        display: false
+		                    },
+		                    ticks: {
+		                        beginAtZero:true,
+		                        fontColor: '#888da8'
+		                    }
+		                }]
+		            }
+		        }
+		    });
+		}
+	}
 }
+
+/** initSwiper 설정 함수 */
+function myInitSwiper() {
+	var swipers = {};
+	var initIterator = 0;
+	var $breakPoints = false;
+	$('.swiper-container').each(function () {
+
+		var $t = $(this);
+		var index = 'swiper-unique-id-' + initIterator;
+
+		$t.addClass('swiper-' + index + ' initialized').attr('id', index);
+		$t.find('.swiper-pagination').addClass('pagination-' + index);
+
+		var $effect = ($t.data('effect')) ? $t.data('effect') : 'slide',
+			$crossfade = ($t.data('crossfade')) ? $t.data('crossfade') : true,
+			$loop = ($t.data('loop') == false) ? $t.data('loop') : true,
+			$showItems = ($t.data('show-items')) ? $t.data('show-items') : 1,
+			$scrollItems = ($t.data('scroll-items')) ? $t.data('scroll-items') : 1,
+			$scrollDirection = ($t.data('direction')) ? $t.data('direction') : 'horizontal',
+			$mouseScroll = ($t.data('mouse-scroll')) ? $t.data('mouse-scroll') : false,
+			$autoplay = ($t.data('autoplay')) ? parseInt($t.data('autoplay'), 10) : 0,
+			$autoheight = ($t.hasClass('auto-height')) ? true: false,
+			$slidesSpace = ($showItems > 1) ? 20 : 0;
+
+		if ($showItems > 1) {
+			$breakPoints = {
+				480: {
+					slidesPerView: 1,
+					slidesPerGroup: 1
+				},
+				768: {
+					slidesPerView: 2,
+					slidesPerGroup: 2
+				}
+			}
+		}
+
+		swipers['swiper-' + index] = new Swiper('.swiper-' + index, {
+			pagination: '.pagination-' + index,
+			paginationClickable: true,
+			direction: $scrollDirection,
+			mousewheelControl: $mouseScroll,
+			mousewheelReleaseOnEdges: $mouseScroll,
+			slidesPerView: $showItems,
+			slidesPerGroup: $scrollItems,
+			spaceBetween: $slidesSpace,
+			keyboardControl: true,
+			setWrapperSize: true,
+			preloadImages: true,
+			updateOnImagesReady: true,
+			autoplay: $autoplay,
+			autoHeight: $autoheight,
+			loop: $loop,
+			breakpoints: $breakPoints,
+			effect: $effect,
+			fade: {
+				crossFade: $crossfade
+			},
+			parallax: true,
+			onSlideChangeStart: function (swiper) {
+			    var sliderThumbs = $t.siblings('.slider-slides');
+				if (sliderThumbs.length) {
+                    sliderThumbs.find('.slide-active').removeClass('slide-active');
+					var realIndex = swiper.slides.eq(swiper.activeIndex).attr('data-swiper-slide-index');
+                    sliderThumbs.find('.slides-item').eq(realIndex).addClass('slide-active');
+				}
+			}
+		});
+		initIterator++;
+	});
+	
+
+    //swiper arrows
+    $('.btn-prev').on('click', function () {
+        var sliderID = $(this).closest('.slider-slides').siblings('.swiper-container').attr('id');
+        swipers['swiper-' + sliderID].slidePrev();
+    });
+
+    $('.btn-next').on('click', function () {
+        var sliderID = $(this).closest('.slider-slides').siblings('.swiper-container').attr('id');
+        swipers['swiper-' + sliderID].slideNext();
+    });
+	
+    //swiper arrows
+    $('.btn-prev-without').on('click', function () {
+        var sliderID = $(this).closest('.swiper-container').attr('id');
+        swipers['swiper-' + sliderID].slidePrev();
+    });
+
+    $('.btn-next-without').on('click', function () {
+        var sliderID = $(this).closest('.swiper-container').attr('id');
+        swipers['swiper-' + sliderID].slideNext();
+    });
+	
+	
+    // Click on thumbs
+    $('.slider-slides .slides-item').on('click', function () {
+        if ($(this).hasClass('slide-active')) return false;
+        var activeIndex = $(this).parent().find('.slides-item').index(this);
+        var sliderID = $(this).closest('.slider-slides').siblings('.swiper-container').attr('id');
+        swipers['swiper-' + sliderID].slideTo(activeIndex + 1);
+        $(this).parent().find('.slide-active').removeClass('slide-active');
+        $(this).addClass('slide-active');
+
+        return false;
+    });
+};
+
 
 function numberWithCommas(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
